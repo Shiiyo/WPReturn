d203d46075cf5d6b71e654d7e1263b14a06eaab3
Camille <camillelegalland@gmail.com>
2022-07-12 22:10:17 +0200

    create a complete personnal theme

--- web/app/themes/montheme/functions.php
+++ web/app/themes/montheme/functions.php
@@ -1,6 +1,63 @@
 <?php
 
-use Timber\Timber;
+function montheme_supports()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En-tête du menu');
 
-$timber = new \Timber\Timber();
-Timber::$autoescape = true;
+    add_image_size('post-thumbnail', 416, 300, true);
+}
+
+function montheme_register_assets()
+{
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [], false, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+function montheme_title_separator()
+{
+    return '|';
+}
+
+function montheme_menu_class($classes)
+{
+    $classes[] = 'nav-item';
+    return $classes;
+}
+
+function montheme_menu_link_class($attrs)
+{
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
+
+function montheme_pagination()
+{
+    $pages = paginate_links(['type' => 'array']);
+    if ($pages === null) {
+        return;
+    }
+
+    echo '<nav aria-label="Pagination">';
+    echo '<ul class="pagination my-4">';
+
+    foreach($pages as $page){
+        $active = strpos($page, 'current') !== false;
+        $class = 'page-item';
+        if ($active) {
+            $class .= ' active';
+        }
+        echo '<li class="' . $class . '">'.str_replace('page-numbers', 'page-link', $page).'</li>';
+    }
+    echo '</ul> </nav>';
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
+add_filter('document_title_separator', 'montheme_title_separator');
+add_filter('nav_menu_css_class', 'montheme_menu_class');
+add_filter('nav_menu_link_attributes', 'montheme_menu_link_class');
--- web/app/themes/montheme/index.php
+++ web/app/themes/montheme/index.php
@@ -1,10 +1,25 @@
-<?php
+<?php get_header() ?>
+<div class="row">
 
-use JupiterX_Core\Raven\Modules\Forms\Fields\Time;
-use Timber\Timber;
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card">
+                    <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-imp-top', 'alt' => '', 'style' => 'height : auto;']); ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text"><?php the_excerpt() ?></p>
+                        <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                    </div>
+                </div>
+            </div>
+    <?php endwhile; ?>
 
-$context = Timber::get_context();
-$context['firstname'] = 'Camille';
-$context['posts'] = Timber::query_posts();
+    <?php montheme_pagination() ?>
 
-Timber::render('page/index.twig', [$context]);
+    <?php else : ?>
+        <p>Aucun article :(</p>
+    <?php endif; ?>
+</div>
+
+<?php get_footer() ?>
